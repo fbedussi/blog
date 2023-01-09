9b6e2a3c28e4aebc9bcebe62debd3910acefc0ac
Francesco Bedussi <francesco.bedussi@abstract.it>
2023-01-09 23:18:48 +0100

    feat: copy code button

---
+++ assets/scripts/copyCode.js
@@ -0,0 +1,34 @@
+// This assumes that you're using Rouge; if not, update the selector
+const codeBlocks = Array.from(
+  document.querySelectorAll('div.highlighter-rouge'),
+)
+
+codeBlocks.forEach((codeBlock) => {
+  const buttonWrapper = document.createElement('div')
+  buttonWrapper.className = 'copy-code-button-wrapper'
+
+  const button = document.createElement('button')
+  const buttonText = 'Copy code to clipboard'
+  button.innerText = buttonText
+  button.className = 'copy-code-button'
+
+  button.addEventListener('click', (e) => {
+    const button = e.target
+    const codeBlock = button.parentElement.parentElement.querySelector('code')
+    const code = codeBlock.innerText
+
+    window.navigator.clipboard.writeText(code)
+
+    button.classList.add('copied')
+    button.innerText = 'Copied!'
+
+    setTimeout(() => {
+      button.innerText = buttonText
+      button.classList.remove('copied')
+    }, 2000)
+  })
+
+  buttonWrapper.appendChild(button)
+
+  codeBlock.appendChild(buttonWrapper)
+})
